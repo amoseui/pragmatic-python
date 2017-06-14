f5001678ffb3223959da5d667e21ec28ff44fb6d
amoseui <amoseui@gmail.com>
2017-06-14 19:05:50 +0900

    LeetCode - 1. Two Sum, python3, Time Limit Exceeded

--- solution_test.py
+++ solution_test.py
@@ -11,6 +11,10 @@ class SolutionTest(unittest.TestCase):
         self.assertEqual(1, Solution.sub(3, 2))
         self.assertEqual(-1, Solution.sub(2, 3))
 
+    def test_sum(self):
+        self.assertEqual([0, 1], Solution.twoSum([2, 7, 11, 15], 9))
+        self.assertEqual([1, 2], Solution.twoSum([3, 2, 4], 6))
+
 
 if __name__ == '__main__':
     unittest.main()
